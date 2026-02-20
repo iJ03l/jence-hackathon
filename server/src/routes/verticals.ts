70f781fd0ab9544e7909662f311bbece53a33578
iJ03l <idahorjoel@gmail.com>
2026-02-20 23:38:58 +0100

    verticals fix

--- server/src/routes/verticals.ts
+++ server/src/routes/verticals.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { db } from '../db/index.js'
 import { vertical, creatorProfile, post, user } from '../db/schema.js'
-import { eq } from 'drizzle-orm'
+import { eq, and } from 'drizzle-orm'
 
 const verticalsRoutes = new Hono()
 
@@ -33,7 +33,13 @@ verticalsRoutes.get('/:slug', async (c) => {
         .from(post)
         .innerJoin(creatorProfile, eq(post.creatorId, creatorProfile.id))
         .innerJoin(user, eq(creatorProfile.userId, user.id))
-        .where(eq(post.verticalId, found.id))
+        .where(
+            and(
+                eq(post.verticalId, found.id),
+                eq(post.isFree, true),
+                eq(post.isPublished, true)
+            )
+        )
         .limit(20)
 
     return c.json({
